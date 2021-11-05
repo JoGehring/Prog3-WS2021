718d44465ed353e61898f8864b75718792bffc5d
JoGehring <92168368+JoGehring@users.noreply.github.com>
2021-11-05 20:25:23 +0100

    exercise 3 v1

--- kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
+++ kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
@@ -98,7 +98,19 @@ std::optional<Column> BoardRepository::postColumn(std::string name, int position
 }
 
 std::optional<Prog3::Core::Model::Column> BoardRepository::putColumn(int id, std::string name, int position) {
-    throw NotImplementedException();
+    string sqlPutColumn = "UPDATE column SET name = " + name + " , position = " + to_string(position) + "WHERE id = " + to_string(id);
+
+    int result = 0;
+    char *errorMessage = nullptr;
+
+    result = sqlite3_exec(database, sqlPutColumn.c_str(), NULL, 0, &errorMessage);
+    handleSQLError(result, errorMessage);
+
+    if (SQLITE_OK == result) {
+        int columnId = sqlite3_last_insert_rowid(database);
+        return Column(columnId, name, position);
+    }
+    return std::nullopt;
 }
 
 void BoardRepository::deleteColumn(int id) {
@@ -150,7 +162,21 @@ std::optional<Item> BoardRepository::postItem(int columnId, std::string title, i
 }
 
 std::optional<Prog3::Core::Model::Item> BoardRepository::putItem(int columnId, int itemId, std::string title, int position) {
-    throw NotImplementedException();
+    string sqlPutItem = "UPDATE item SET name = " + title + " , position = " + to_string(position) + "WHERE id = " + to_string(itemId) +
+                        " AND columnId = " + to_string(columnId);
+    time_t now = time(0);
+    char *datetime = ctime(&now);
+    int result = 0;
+    char *errorMessage = nullptr;
+
+    result = sqlite3_exec(database, sqlPutItem.c_str(), NULL, 0, &errorMessage);
+    handleSQLError(result, errorMessage);
+
+    if (SQLITE_OK == result) {
+        int columnId = sqlite3_last_insert_rowid(database);
+        return Item(itemId, title, position, datetime);
+    }
+    return std::nullopt;
 }
 
 void BoardRepository::deleteItem(int columnId, int itemId) {
