471d7d0f91db2f5750d1b4bb32ff030b0bd44aca
JoGehring <92168368+JoGehring@users.noreply.github.com>
2021-11-08 08:02:22 +0100

    exercise 3 v15

--- kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
+++ kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
@@ -112,8 +112,8 @@ std::optional<Prog3::Core::Model::Column> BoardRepository::putColumn(int id, std
     handleSQLError(result, errorMessage);
 
     if (SQLITE_OK == result) {
-        int columnId = sqlite3_last_insert_rowid(database);
-        return Column(columnId, name, position);
+        //int columnId = sqlite3_last_insert_rowid(database);
+        return Column(id, name, position);
     }
     return std::nullopt;
 }
