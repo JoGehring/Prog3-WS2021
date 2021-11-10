f69c03f7d1c1dbdeedd145b3c323405f881bde81
JoGehring <92168368+JoGehring@users.noreply.github.com>
2021-11-10 16:59:43 +0100

    exercise 3 v21

--- kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
+++ kanban-board-service/src/Repository/SQLite/BoardRepository.cpp
@@ -139,7 +139,7 @@ void BoardRepository::deleteColumn(int id) {
 
 std::vector<Item> BoardRepository::getItems(int columnId) {
     string sqlGetItems =
-        "SELECT id, title, position, date FROM item WHERE column_id = '" + to_string(columnId) + "'";
+        "SELECT id, title, position, date FROM item JOIN column ON column.id = " + to_string(columnId);
 
     int result = 0;
     char *errormessage = nullptr;
